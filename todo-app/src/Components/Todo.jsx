24b9552e0c2ca9c99f8a30c1eba01d788b56262b
SykaBliet <aurele.guitard@gmail.com>
2021-07-28 12:21:34 +0200

    Done check fonctionality

--- todo-app/src/Components/Todo.jsx
+++ todo-app/src/Components/Todo.jsx
@@ -1,27 +1,33 @@
 import React from "react";
 
 function Todo({ text, id, isActive, completed, todo, todos, setTodos }) {
-
   const deleteHandler = () => {
-    setTodos(todos.filter(el => el.id !== todo.id))
+    setTodos(todos.filter((el) => el.id !== todo.id));
   };
 
-  
   const completedHandler = () => {
-    setTodos(todos.map(el => {
-      if (el.id === todo.id) {
-        return {
-          ...el, completed: !el.completed , isActive: !el.isActive
+    setTodos(
+      todos.map((el) => {
+        if (el.id === todo.id) {
+          return {
+            ...el,
+            completed: !el.completed,
+            isActive: !el.isActive,
+          };
         }
-      }
-      return el;
-    }
-    ))
-  }
+        return el;
+      })
+    );
+  };
 
   return (
     <div>
-      <input className="flex items-center justify-center" type="text" defaultValue={text} />
+      <input
+        className={`flex items-center justify-center 
+        ${todo.completed ? "text-gray-400" : "text-current"}`}
+        type="text"
+        defaultValue={text}
+      />
       <button onClick={deleteHandler}>Del</button>
       <button onClick={completedHandler}>Done</button>
     </div>
